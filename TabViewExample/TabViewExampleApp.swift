8a3a622fcca8c513a11aafeb81371272738633d1
Alexander Khitev <alexsanderkhitev@gmail.com>
2022-08-14 16:25:25 +0400

    Initial Commit

---
+++ TabViewExample/TabViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewExampleApp.swift
+//  TabViewExample
+//
+//  Created by Alexander Khitev on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
